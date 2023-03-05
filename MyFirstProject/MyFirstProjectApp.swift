b8884604ff93afe6e089aab804e071cccb9ae222
Nurlan Seitov <seitov@gmail.com>
2023-03-05 22:19:45 +0600

    Initial Commit

---
+++ MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Nurlan Seitov on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
